7e4b0668213a879290cab50c6fb0511abb317031
arfixfari@gmail.com <arfixfari@gmail.com>
2017-09-01 19:40:53 +0700

    Heroku

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from datetime import date
 
 # Enter your name here
-mhs_name = '' # TODO Implement this
+mhs_name = 'Fari Qodri Andana' # TODO Implement this
 
 # Create your views here.
 def index(request):
-    response = {'name': mhs_name}
+    response = {'name': mhs_name, 'age' : calculate_age(1999)}
     return render(request, 'index.html', response)
 
 # TODO Implement this to complete last checklist
 def calculate_age(birth_year):
-    pass
+    current = date.today().year
+    return current - birth_year
