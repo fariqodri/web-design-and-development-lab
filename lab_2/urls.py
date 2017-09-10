1f5de6d26149fcf85a0487057f3c6b830c5a8857
fari.qodri <arfixfari@gmail.com>
2017-09-10 18:24:07 +0700

    Test Lab 2 Error

--- lab_2/urls.py
+++ lab_2/urls.py
@@ -4,6 +4,5 @@ from .views import index
 #url for app, add your URL Configuration
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^lab-2/', include(lab_2,namespace='lab-2')),   
+    url(r'^lab-2/', index, name='index'),  
 ]
