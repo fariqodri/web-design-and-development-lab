cf92f3df5714be612dc7b8e33910c198a9f76019
fari.qodri <arfixfari@gmail.com>
2017-09-10 18:21:15 +0700

    Test Lab 2 Error

--- lab_2/urls.py
+++ lab_2/urls.py
@@ -4,5 +4,6 @@ from .views import index
 #url for app, add your URL Configuration
 
 urlpatterns = [
-    url(r'^$', index, name='index'),    
+    url(r'^$', index, name='index'),
+    url(r'^lab-2/', include(lab_2,namespace='lab-2')),   
 ]
--- lab_2/views.py
+++ lab_2/views.py
@@ -3,7 +3,7 @@ from lab_1.views import mhs_name, birth_date
 
 #TODO Implement
 #Create a content paragraph for your landing page:
-landing_page_content = 'Welcome to my website. Please excuse any sort of flaws or imperfection. I\'m still learning'
+landing_page_content = 'Welcome to my website. Please excuse any sort of flaws or imperfection.'
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
