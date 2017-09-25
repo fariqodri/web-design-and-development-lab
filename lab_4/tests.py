7e7770d197d438f206d89f604faa3e400bdb4904
farqodri <arfixfari@gmail.com>
2017-09-25 11:12:19 +0700

    Coba

--- lab_4/tests.py
+++ lab_4/tests.py
@@ -6,52 +6,97 @@ from .views import index, about_me, landing_page_content
 from lab_1.views import mhs_name
 from .models import Message
 from .forms import Message_Form
+from .views import index, message_post
+from .views import index, message_table, about_me, landing_page_content
 
 # Create your tests here.
 
 class Lab4UnitTest(TestCase):
-    def test_lab_4_url_is_exist(self):
-        response = Client().get('/lab-4/')
-        self.assertEqual(response.status_code, 200)
+	def test_lab_4_url_is_exist(self):
+		response = Client().get('/lab-4/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_about_me_more_than_6(self):
-       self.assertTrue(len(about_me) >= 6)
+	def test_about_me_more_than_6(self):
+		self.assertTrue(len(about_me) >= 6)
 
-    def test_lab4_using_index_func(self):
-        found = resolve('/lab-4/')
-        self.assertEqual(found.func, index)
+	def test_lab4_using_index_func(self):
+		found = resolve('/lab-4/')
+		self.assertEqual(found.func, index)
 
-    def test_landing_page_is_completed(self):
-        request = HttpRequest()
-        response = index(request)
-        html_response = response.content.decode('utf8')
+	def test_landing_page_is_completed(self):
+		request = HttpRequest()
+		response = index(request)
+		html_response = response.content.decode('utf8')
 
         #Checking whether have Bio content
-        self.assertIn(landing_page_content, html_response)
+		self.assertIn(landing_page_content, html_response)
 
         #Chceking whether all About Me Item is rendered
-        for item in about_me:
-            self.assertIn(item,html_response)
+		for item in about_me:
+			self.assertIn(item,html_response)
 
-    def test_model_can_create_new_message(self):
-        #Creating a new activity
-        new_activity = Message.objects.create(name=mhs_name,email='test@gmail.com',message='This is a test')
+	def test_model_can_create_new_message(self):
+		#Creating a new activity
+		new_activity = Message.objects.create(name=mhs_name,email='test@gmail.com',message='This is a test')
 
-        #Retrieving all available activity
-        counting_all_available_message= Message.objects.all().count()
-        self.assertEqual(counting_all_available_message,1)
+		#Retrieving all available activity
+		counting_all_available_message= Message.objects.all().count()
+		self.assertEqual(counting_all_available_message,1)
 
-    def test_form_message_input_has_placeholder_and_css_classes(self):
-        form = Message_Form()
-        self.assertIn('class="form-control"', form.as_p())
-        self.assertIn('<label for="id_name">Nama:</label>', form.as_p())
-        self.assertIn('<label for="id_email">Email:</label>', form.as_p())
-        self.assertIn('<label for="id_message">Message:</label>', form.as_p())
+	def test_form_message_input_has_placeholder_and_css_classes(self):
+		form = Message_Form()
+		self.assertIn('class="form-control"', form.as_p())
+		self.assertIn('<label for="id_name">Nama:</label>', form.as_p())
+		self.assertIn('<label for="id_email">Email:</label>', form.as_p())
+		self.assertIn('<label for="id_message">Message:</label>', form.as_p())
 
-    def test_form_validation_for_blank_items(self):
-        form = Message_Form(data={'name': '', 'email': '', 'message': ''})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['message'],
-            ["This field is required."]
-        )
+	def test_form_validation_for_blank_items(self):
+		form = Message_Form(data={'name': '', 'email': '', 'message': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['message'],
+			["This field is required."]
+		)
+		
+	def test_lab4_post_fail(self):
+		response = Client().post('/lab-4/add_message', {'name': 'Anonymous', 'email': 'A', 'message': ''})
+		self.assertEqual(response.status_code, 302)
+
+	def test_lab4_post_success_and_render_the_result(self):
+		anonymous = 'Anonymous'
+		message = 'HaiHai'
+		response = Client().post('/lab-4/add_message', {'name': '', 'email': '', 'message': message})
+		self.assertEqual(response.status_code, 200)
+		html_response = response.content.decode('utf8')
+		self.assertIn(anonymous,html_response)
+		self.assertIn(message,html_response)
+	
+	def test_lab_4_table_url_exist(self):
+		response = Client().get('/lab-4/result_table')
+		self.assertEqual(response.status_code, 200)
+
+	def test_lab_4_table_using_message_table_func(self):
+		found = resolve('/lab-4/result_table')
+		self.assertEqual(found.func, message_table)
+
+	def test_lab_4_showing_all_messages(self):
+		name_budi = 'Budi'
+		email_budi = 'budi@ui.ac.id'
+		message_budi = 'Lanjutkan Kawan'
+		data_budi = {'name': name_budi, 'email': email_budi, 'message': message_budi}
+		post_data_budi = Client().post('/lab-4/add_message', data_budi)
+		self.assertEqual(post_data_budi.status_code, 200)
+
+		message_anonymous = 'Masih Jelek Nih'
+		data_anonymous = {'name': '', 'email': '', 'message': message_anonymous}
+		post_data_anonymous = Client().post('/lab-4/add_message', data_anonymous)
+		self.assertEqual(post_data_anonymous.status_code, 200)
+
+		response = Client().get('/lab-4/result_table')
+		html_response = response.content.decode('utf8')
+
+		for key,data in data_budi.items():
+			self.assertIn(data,html_response)
+
+		self.assertIn('Anonymous', html_response)
+		self.assertIn(message_anonymous, html_response)
--- lab_4/views.py
+++ lab_4/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from lab_2.views import landing_page_content
+from django.http import HttpResponseRedirect
+from .forms import Message_Form
+from .models import Message
 
 # Create your views here.
 response = {'author': "Fari Qodri Andana"} #TODO Implement yourname
 about_me = ['aneh', 'sehat', 'kuat', 'macan', 'biskuat', 'tiger']
+
 def index(request):
-    response['content'] = landing_page_content
-    html = 'lab_4/lab_4.html'
-    #TODO Implement, isilah dengan 6 kata yang mendeskripsikan anda
-    response['about_me'] = about_me
-    return render(request, html, response)
+	response['content'] = landing_page_content
+	html = 'lab_4/lab_4.html'
+	#TODO Implement, isilah dengan 6 kata yang mendeskripsikan anda
+	response['about_me'] = about_me
+	response['message_form'] = Message_Form
+	return render(request, html, response)
+
+def message_post(request):
+	form = Message_Form(request.POST or None)
+	if(request.method == 'POST' and form.is_valid()):
+		response['name'] = request.POST['name'] if request.POST['name'] != "" else "Anonymous"
+		response['email'] = request.POST['email'] if request.POST['email'] != "" else "Anonymous"
+		response['message'] = request.POST['message']
+		message = Message(name=response['name'], email=response['email'],
+						message=response['message'])
+		message.save()
+		html ='lab_4/form_result.html'
+		return render(request, html, response)
+	else:        
+		return HttpResponseRedirect('/lab-4/')
+
+def message_table(request):
+	message = Message.objects.all()
+	response['message'] = message
+	html = 'lab_4/table.html'
+	return render(request, html , response)
+
+
+
