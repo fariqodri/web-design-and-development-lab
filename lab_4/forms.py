f638e597bf81ef2065376c95e1f6096c8b2dcb1b
fari.qodri <arfixfari@gmail.com>
2017-11-06 20:13:40 +0700

    JS

--- lab_4/forms.py
+++ lab_4/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
 class Message_Form(forms.Form):
-<<<<<<< HEAD
     error_messages = {
         'required': 'Tolong isi input ini',
         'invalid': 'Isi input dengan email',
@@ -13,16 +12,3 @@ class Message_Form(forms.Form):
     name = forms.CharField(label='Nama', required=False, max_length=27, empty_value='Anonymous', widget=forms.TextInput(attrs=attrs))
     email = forms.EmailField(required=False, widget=forms.EmailInput(attrs=attrs))
     message = forms.CharField(widget=forms.Textarea(attrs=attrs), required=True)
-=======
-	error_messages = {
-		'required': 'Tolong isi input ini',
-		'invalid': 'Isi input dengan email',
-	}
-	attrs = {
-		'class': 'form-control'
-	}
-
-	name = forms.CharField(label='Nama', required=False, max_length=27, empty_value='Anonymous', widget=forms.TextInput(attrs=attrs))
-	email = forms.EmailField(required=False, widget=forms.EmailInput(attrs=attrs))
-	message = forms.CharField(widget=forms.Textarea(attrs=attrs), required=True)
->>>>>>> db51b323b861f141bcde36ffbb8f8792ba697b2b
