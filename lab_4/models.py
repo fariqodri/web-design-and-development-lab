5d4bc3b25ea61439cfe60ba48e2baa073671b475
fari.qodri <arfixfari@gmail.com>
2017-11-06 20:08:31 +0700

    JS

--- lab_4/models.py
+++ lab_4/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 class Message(models.Model):
-<<<<<<< HEAD
-    name = models.CharField(max_length=27)
-    email = models.EmailField()
-    message = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.message
-=======
 	name = models.CharField(max_length=27)
 	email = models.EmailField()
 	message = models.TextField()
@@ -17,4 +8,3 @@ class Message(models.Model):
 
 	def __str__(self):
 		return self.message
->>>>>>> db51b323b861f141bcde36ffbb8f8792ba697b2b
