e817695e9de8ba4f2d67fc66d7531cff9a4226dd
farqodri <arfixfari@gmail.com>
2017-11-21 07:55:41 +0700

    Ignore VS Code and add Tests

--- lab_8/tests.py
+++ lab_8/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from django.test import Client
+from django.urls import resolve
 # Create your tests here.
+class Lab8UnitTest(TestCase):
+	def test_lab_8_url_is_exist(self):
+		response = Client().get('/lab-8/')
+		self.assertEqual(response.status_code, 200)
