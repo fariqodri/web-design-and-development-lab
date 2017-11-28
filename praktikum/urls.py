369d088c75d879105f3d7c3dbbc0419c2dcc2056
farqodri <arfixfari@gmail.com>
2017-11-28 13:41:42 +0700

    Lab 7 and CSUI Helper

--- praktikum/urls.py
+++ praktikum/urls.py
@@ -22,7 +22,7 @@ import lab_3.urls as lab_3
 import lab_4.urls as lab_4
 import lab_5.urls as lab_5
 import lab_6.urls as lab_6
-#import lab_7.urls as lab_7
+import lab_7.urls as lab_7
 import lab_8.urls as lab_8
 import lab_9.urls as lab_9
 from django.views.generic.base import RedirectView
@@ -38,7 +38,7 @@ urlpatterns = [
 	url(r'^lab-4/', include(lab_4, namespace='lab-4')),
 	url(r'^lab-5/', include(lab_5, namespace='lab-5')),
 	url(r'^lab-6/', include(lab_6, namespace='lab-6')),
-	#url(r'^lab-7/', include(lab_7, namespace='lab-7')),
+	url(r'^lab-7/', include(lab_7, namespace='lab-7')),
 	url(r'^lab-8/', include(lab_8, namespace='lab-8')),
 	url(r'^lab-9/', include(lab_9, namespace='lab-9')),
 ]
