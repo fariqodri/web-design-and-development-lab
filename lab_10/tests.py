f1c3a4232d213ad051d1bd9d921906e3944fde66
farqodri <arfixfari@gmail.com>
2017-12-05 14:24:54 +0700

    Test logout

--- lab_10/tests.py
+++ lab_10/tests.py
@@ -10,38 +10,38 @@ environ.Env.read_env('.env')
 
 # Create your tests here.
 class Lab10UnitTest(TestCase):
-    def test_logout(self):
-        response = self.client.post('/lab-10/custom_auth/login/', {'username': self.username, 'password': self.password})
-        self.assertEqual(response.status_code, 302)
-        response = self.client.post('/lab-10/custom_auth/logout/')
-        html_response = self.client.get('/lab-10/').content.decode('utf-8')
-        self.assertEqual(response.status_code, 302)
-        self.assertIn("Anda berhasil logout. Semua session Anda sudah dihapus", html_response)
+	def test_logout(self):
+		response = self.client.post('/lab-10/custom_auth/login/', {'username': self.username, 'password': self.password})
+		self.assertEqual(response.status_code, 302)
+		response = self.client.post('/lab-10/custom_auth/logout/')
+		html_response = self.client.get('/lab-10/').content.decode('utf-8')
+		self.assertEqual(response.status_code, 302)
+		self.assertIn("Anda berhasil logout. Semua session Anda sudah dihapus", html_response)
 
-    def setUp(self):
-	    self.username = env("SSO_USERNAME")
-	    self.password = env("SSO_PASSWORD")
+	def setUp(self):
+		self.username = env("SSO_USERNAME")
+		self.password = env("SSO_PASSWORD")
 
-    def test_login_failed(self):
-        response = self.client.post('/lab-10/custom_auth/login/', {'username': "siapa", 'password': "saya"})
-        html_response = self.client.get('/lab-10/').content.decode('utf-8')
-        self.assertIn("Username atau password salah", html_response)
+	def test_login_failed(self):
+		response = self.client.post('/lab-10/custom_auth/login/', {'username': "siapa", 'password': "saya"})
+		html_response = self.client.get('/lab-10/').content.decode('utf-8')
+		self.assertIn("Username atau password salah", html_response)
 
-    def test_lab_10_url_is_exist(self):
-        response = self.client.get('/lab-10/')
-        self.assertEqual(response.status_code, 200)
+	def test_lab_10_url_is_exist(self):
+		response = self.client.get('/lab-10/')
+		self.assertEqual(response.status_code, 200)
 
-    def test_lab_10_page_when_user_is_logged_in_and_otherwise(self):
-        #not logged in, render login template
-        response = self.client.get('/lab-10/')
-        html_response = response.content.decode('utf-8')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('lab_10/login.html')
-        #logged in, redirect to dashboard
-        self.client.post('/lab-10/custom_auth/login/', {'username': self.username, 'password': self.password})
-        response = self.client.get('/lab-10/')
-        self.assertEqual(response.status_code, 302)
-        self.assertTemplateUsed('lab_10/dashboard.html')
+	def test_lab_10_page_when_user_is_logged_in_and_otherwise(self):
+		#not logged in, render login template
+		response = self.client.get('/lab-10/')
+		html_response = response.content.decode('utf-8')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed('lab_10/login.html')
+		#logged in, redirect to dashboard
+		self.client.post('/lab-10/custom_auth/login/', {'username': self.username, 'password': self.password})
+		response = self.client.get('/lab-10/')
+		self.assertEqual(response.status_code, 302)
+		self.assertTemplateUsed('lab_10/dashboard.html')
 
 	def test_direct_acces_to_dashboard_url(self):
 		#not logged in, redirect to login page
