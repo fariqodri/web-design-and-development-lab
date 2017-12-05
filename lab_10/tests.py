ea55c65f1747244cb4832bb60b7f0884f4b89937
farqodri <arfixfari@gmail.com>
2017-12-05 14:01:28 +0700

    Test logout

--- lab_10/tests.py
+++ lab_10/tests.py
@@ -10,14 +10,14 @@ environ.Env.read_env('.env')
 
 # Create your tests here.
 class Lab10UnitTest(TestCase):
-    def test_logout(self):
+	def test_logout(self):
 		response = self.client.post('/lab-9/custom_auth/login/', {'username': self.username, 'password': self.password})
 		self.assertEqual(response.status_code, 302)
 		response = self.client.post('/lab-9/custom_auth/logout/')
 		html_response = self.client.get('/lab-9/').content.decode('utf-8')
 		self.assertEqual(response.status_code, 302)
 		self.assertIn("Anda berhasil logout. Semua session Anda sudah dihapus", html_response)
-        
+
 	def setUp(self):
 		self.username = env("SSO_USERNAME")
 		self.password = env("SSO_PASSWORD")
